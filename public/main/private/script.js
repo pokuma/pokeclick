ef58be16eb3fb2737911384de476a23f60cdd057
im-somnus <somnusanimus@gmail.com>
2023-03-25 15:54:49 +0100

    Custom cursor on click + clickable items!
    - Added a custom cursor (from maplestory cursor)
    - Items with the class  clickable have an animation to show its clickable.

--- public/main/private/script.js
+++ public/main/private/script.js
@@ -1,5 +1,17 @@
 let pokemonList = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran", "Nidorina", "Nidoqueen", "Nidoran", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetch'd", "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie", "Mr. Mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo", "Mew"];
 
+// Cursor
+document.getElementById("body").addEventListener("onmousedown", function (e) { document.getElementById("body").style.cursor.URL = "/clickdown.gif"; });
+
+var cursor = document.getElementsByClassName('clicked')[0]
+cursor.onmousedown = function () {
+    cursor.classList.add('clickdown')
+}
+cursor.onmouseup = function () {
+    cursor.classList.remove('clickdown')
+}
+
+
 // Pokemon health points bar
 const hpBar = document.getElementById("hpBar");
 const yellowZone = hpBar.ariaValueMax / 1.5;
@@ -68,7 +80,6 @@ function attack() {
                 hpBar.classList = "progress-bar progress-bar-striped progress-bar-animated bg-warning";
             }
         }
-
         hpBar.ariaValueNow -= dmg;
         hpBarText.innerHTML = "hp: " + hpBar.ariaValueNow + " / " + hpBar.ariaValueMax;
         hpBar.style.width = "" + hpBar.ariaValueNow + "%";
