6410809867a65603990607f03d1b6d2bd391b6ee
VictorMadrona <victormadrona@uma.es>
2023-03-26 12:52:39 +0200

    html reorganized in a subdirectory

--- public/main/private/script.js
+++ public/main/private/script.js
@@ -1,7 +1,7 @@
 let pokemonList = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran", "Nidorina", "Nidoqueen", "Nidoran", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetch'd", "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie", "Mr. Mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo", "Mew"];
 
 // Cursor
-document.getElementById("body").addEventListener("onmousedown", function(e) { document.getElementById("body").style.cursor.URL = "/clickdown.gif"; });
+document.getElementById("body").addEventListener("onmousedown", function(e) { document.getElementById("body").style.cursor.URL = "../images/cursor/clickdown.gif"; });
 
 var cursor = document.getElementsByClassName('clicked')[0]
 cursor.onmousedown = function() {
@@ -132,7 +132,7 @@ function stopPokemonShake() {
 function spawnPokemon() {
     let pokeSize = pokemonList.length;
     let pokemonNumber = Math.floor(Math.random() * (pokeSize - 1));
-    pokemon.src = "assets/images/pokemon/" + (pokemonNumber + 1) + ".png";
+    pokemon.src = "../assets/images/pokemon/" + (pokemonNumber + 1) + ".png";
     pokeName.innerHTML = (pokemonList[pokemonNumber].toLowerCase());
 }
 
