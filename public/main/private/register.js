fd5e2be9d267fda8017a9fefaeb0ba9262750b7d
Carlos-DE <carlosdominguezenriquez611@gmail.com>
2023-03-26 14:50:07 +0200

    register contraseñas no cooinciden

--- public/main/private/register.js
+++ public/main/private/register.js
@@ -2,10 +2,17 @@ function validateForm() {
     const nameInput = document.getElementById("nicknames");
     const emailInput = document.getElementById("emails");
     const passwordInput = document.getElementById("passwords");
+    const reppasswordInput = document.getElementById("reppasswords");
 
     if (nameInput.value === "" || emailInput.value === "" || passwordInput.value === "") {
         alert("Por favor, rellene todos los campos requeridos.");
         return false;
     }
+
+    if (passwordInput != reppasswordInput) {
+        alert("Las contraseñas no coinciden.");
+        return false;
+    }
+
     return true;
 }
