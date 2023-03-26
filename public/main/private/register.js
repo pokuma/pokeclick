c1ba0b9abc38490b89aae33b385680229914326b
im-somnus <somnusanimus@gmail.com>
2023-03-26 15:34:48 +0200

    Fixed register password validation check.
    - There was an error, was checking if the objects where the same, not the string. FIXED.

--- public/main/private/register.js
+++ public/main/private/register.js
@@ -4,12 +4,13 @@ function validateForm() {
     const passwordInput = document.getElementById("passwords");
     const reppasswordInput = document.getElementById("reppasswords");
 
+
     if (nameInput.value === "" || emailInput.value === "" || passwordInput.value === "") {
         alert("Por favor, rellene todos los campos requeridos.");
         return false;
     }
 
-    if (passwordInput != reppasswordInput) {
+    if (passwordInput.value != reppasswordInput.value) {
         alert("Las contraseñas no coinciden.");
         return false;
     }
