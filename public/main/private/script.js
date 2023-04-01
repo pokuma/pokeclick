3315c5da0dea770deffdfccc2f0dfe2a441b4cec
im-somnus <somnusanimus@gmail.com>
2023-04-02 01:07:47 +0200

    Fixed onmouse down cursor error on register.html and login.html

--- public/main/private/script.js
+++ public/main/private/script.js
@@ -1,12 +1,15 @@
 // Cursor
-document.getElementById("body").addEventListener("onmousedown", function(e) { document.getElementById("body").style.cursor.URL = "../images/cursor/clickdown.gif"; });
+document.getElementById("body").addEventListener("onmousedown", function (e) { document.getElementById("body").style.cursor.URL = "../images/cursor/clickdown.gif"; });
 
 var cursor = document.getElementsByClassName('clicked')[0]
-cursor.onmousedown = function() {
-    cursor.classList.add('clickdown')
-}
-cursor.onmouseup = function() {
-    cursor.classList.remove('clickdown')
+
+if (cursor !== null && cursor !== undefined) {
+    cursor.onmousedown = function () {
+        cursor.classList.add('clickdown')
+    }
+    cursor.onmouseup = function () {
+        cursor.classList.remove('clickdown')
+    }
 }
 
 // Function that plays the sound of the file url we send as parameter
@@ -15,7 +18,7 @@ function playAudio(url) {
 }
 
 function changeDarkMode() {
-    
+
     var body = document.getElementById("body");
     body.classList.toggle("dark-mode");
 
@@ -36,7 +39,7 @@ function loadDarkMode() {
         var isDarkModeEnabled = darkModeCookie.split("=")[1] === "true";
         var body = document.getElementById("body");
         body.classList.toggle("dark-mode", isDarkModeEnabled);
-        
-        
+
+
     }
 }
