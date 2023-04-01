96f77683b94913abadae5c6b7d87e8172fa9cf77
antonioortegas <antonioortega@uma.es>
2023-04-01 23:40:35 +0200

    caught pokemon array moved into player object

--- public/main/private/game.js
+++ public/main/private/game.js
@@ -1,12 +1,11 @@
 var pokemonList = ["bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon", "charizard", "squirtle", "wartortle", "blastoise", "caterpie", "metapod", "butterfree", "weedle", "kakuna", "beedrill", "pidgey", "pidgeotto", "pidgeot", "rattata", "raticate", "spearow", "fearow", "ekans", "arbok", "pikachu", "raichu", "sandshrew", "sandslash", "nidoran♀", "nidorina", "nidoqueen", "nidoran♂", "nidorino", "nidoking", "clefairy", "clefable", "vulpix", "ninetales", "jigglypuff", "wigglytuff", "zubat", "golbat", "oddish", "gloom", "vileplume", "paras", "parasect", "venonat", "venomoth", "diglett", "dugtrio", "meowth", "persian", "psyduck", "golduck", "mankey", "primeape", "growlithe", "arcanine", "poliwag", "poliwhirl", "poliwrath", "abra", "kadabra", "alakazam", "machop", "machoke", "machamp", "bellsprout", "weepinbell", "victreebel", "tentacool", "tentacruel", "geodude", "graveler", "golem", "ponyta", "rapidash", "slowpoke", "slowbro", "magnemite", "magneton", "farfetch’d", "doduo", "dodrio", "seel", "dewgong", "grimer", "muk", "shellder", "cloyster", "gastly", "haunter", "gengar", "onix", "drowzee", "hypno", "krabby", "kingler", "voltorb", "electrode", "exeggcute", "exeggutor", "cubone", "marowak", "hitmonlee", "hitmonchan", "lickitung", "koffing", "weezing", "rhyhorn", "rhydon", "chansey", "tangela", "kangaskhan", "horsea", "seadra", "goldeen", "seaking", "staryu", "starmie", "mr.mime", "scyther", "jynx", "electabuzz", "magmar", "pinsir", "tauros", "magikarp", "gyarados", "lapras", "ditto", "eevee", "vaporeon", "jolteon", "flareon", "porygon", "omanyte", "omastar", "kabuto", "kabutops", "aerodactyl", "snorlax", "articuno", "zapdos", "moltres", "dratini", "dragonair", "dragonite", "mewtwo", "mew"];
 
-var caughtPokemons = [];
-
 let player = {
     xp: 0,
-    level: 1,
+    level: 5,
     money: 0,
-    attacks: 5
+    attacks: 5,
+    caughtPokemons: []
 };
 
 let pokeCounter = 0;
@@ -137,8 +136,8 @@ var alreadyCaught = function (name) {
  *      Restore the bar's hp for the next pokemon
  */
 function pokemonDies() {
-    if (caughtPokemons === undefined || !alreadyCaught(pokeName.innerHTML)) {
-        caughtPokemons.push(pokeName.innerHTML);
+    if (!alreadyCaught(pokeName.innerHTML)) {
+        player.caughtPokemons.push(pokeName.innerHTML);
         pokeCounter++;
         pokeCount.innerHTML = "pokemon caught: " + pokeCounter;
     }
@@ -261,10 +260,10 @@ function resetConfirmed() {
         xp: 0,
         level: 1,
         money: 0,
-        attacks: 5
+        attacks: 5,
+        caughtPokemons: []
     }
 
-    caughtPokemons = [];
     pokeCounter = 0;
 
     save();
